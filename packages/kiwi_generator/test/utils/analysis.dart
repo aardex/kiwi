7a0d86d25c5ad8ce93f367e8d15e39254947e741
Benjamin Lempereur <benjamin.lempereur@aardexgroup.com>
2026-02-04 12:28:11 +0100

    Fix Dart analyzer compatibility

--- packages/kiwi_generator/test/utils/analysis.dart
+++ packages/kiwi_generator/test/utils/analysis.dart
@@ -3,27 +3,21 @@ import 'dart:io';
 
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
-import 'package:path/path.dart' as p;
+import 'package:package_config/package_config.dart';
 import 'package:source_gen/source_gen.dart';
 
 Future<LibraryReader> resolveCompilationUnit(String sourceFile) async {
-  final files = [
-    File(sourceFile),
-  ];
-
-  final fileMap = Map<String, String>.fromEntries(
-    files.map(
-      (f) => MapEntry(
-        'a|lib/${p.basename(f.path)}',
-        f.readAsStringSync(),
-      ),
-    ),
+  final packageConfig = await loadPackageConfigUri(
+    File('.dart_tool/package_config.json').absolute.uri,
   );
 
-  final library = await resolveSources(fileMap, (item) async {
-    final assetId = AssetId.parse(fileMap.keys.first);
-    return await item.libraryFor(assetId);
-  });
+  final assetId = AssetId('kiwi_generator', sourceFile);
+  final library = await resolveAsset(
+    assetId,
+    (resolver) => resolver.libraryFor(assetId),
+    packageConfig: packageConfig,
+    readAllSourcesFromFilesystem: true,
+  );
 
   return LibraryReader(library);
 }
